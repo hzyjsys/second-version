61d6ed54bb3efc7b7beaf690b88812ad9ec1f35e
hzyjsys <hzyj_sys@163.com>
2017-04-18 15:58:45 +0800

    test

---
+++ js/common.js
@@ -0,0 +1,56 @@
+/*
+* @Author: JMW
+* @Date:   2017-03-27 16:43:07
+* @Last Modified by:   JMW
+* @Last Modified time: 2017-04-18 10:10:37
+*/
+'use strict';
+jQuery(document).ready(function($) {
+	//改变窗口大小浏览器自动刷新
+	window.onresize=function(){ 
+        location.reload(); 
+    }; 
+	//logo切换
+    var flag=0;
+    $(".logo").click(function(){
+        if(flag==1){
+            $(".logo").find("img").attr("src","../images/logo2.png");
+            flag=0;
+        }else{
+            $(".logo").find("img").attr("src","../images/logo1.png");
+            flag=1;
+        }
+    });
+	// 导航下拉
+    $('#nav-bar .nav li').mouseenter(function(event) {
+        $(this).addClass('on').siblings('li').removeClass('on');
+        $(this).find('dl').stop().slideDown().parents('li').siblings('li').find('dl').stop().slideUp();
+    }).mouseleave(function(event) {
+        $(this).removeClass('on');
+        $(this).find('dl').stop().slideUp();
+    });
+    //左侧导航栏跟随
+    var explorer =navigator.userAgent;
+    if (explorer.indexOf("MSIE") >= 0||explorer.indexOf("rv:11.0")>=0) {
+        var l=$(".left-nav").offset().left;
+        $(window).scroll( function() {
+            var m =$(document).scrollTop();
+            if(m>570&&$(window).width()>=1200){
+                $(".left-nav").css({"position":"fixed","top":"50px","left":l+"px","marginLeft":"0"});
+            }else if(m>570&&$(window).width()<1200&&l<=0){
+                $(".left-nav").css({"position":"fixed","top":"50px","left":"0"});
+            }else{
+                $(".left-nav").css({"position":"absolute","top":"50px","left":"0"});
+            }
+        });
+    }else{
+        $(window).scroll( function() {
+            var m =$(document).scrollTop();
+            if(m>570){
+                $(".left-nav").css("top",(m-480)+"px");
+            }else{
+                $(".left-nav").css("top","50px");
+            }
+        });
+    }   
+});
